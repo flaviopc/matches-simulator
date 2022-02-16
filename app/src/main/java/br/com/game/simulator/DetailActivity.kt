c1dbf542bcc172270f79c54dd88682586bb5c2e0
Flávio Paixão <flavio_pc@outlook.com>
2022-02-16 09:32:05 -0300

    Criação da tela de detalhes

---
+++ app/src/main/java/br/com/game/simulator/DetailActivity.kt
@@ -0,0 +1,17 @@
+package br.com.game.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.game.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
